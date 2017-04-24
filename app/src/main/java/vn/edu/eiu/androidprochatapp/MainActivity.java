c930d7a6502767aea9457bc82151247d8928e40d
tphu <tphu003200@gmail.com>
2017-04-24 21:48:31 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/eiu/androidprochatapp/MainActivity.java
@@ -0,0 +1,18 @@
+package vn.edu.eiu.androidprochatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String APP_ID = "";
+    static final String AUTH_KEY= "";
+    static final String AUTH_SECRET= "";
+    static final String ACCOUT_KEY = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
